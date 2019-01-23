ca5a1174a9847af1e919789ab2f35125956c1962
Kyle Baron <kyleb@metrumrg.com>
2019-01-23 14:22:13 -0600

    documentation

--- pkg/R/depot.R
+++ pkg/R/depot.R
@@ -1,5 +1,24 @@
-##' Model depot location
+##' Access the model repo
+##'
+##' Pass in the name of a model to load the model; or call with
+##' no arguments to get the path to those models (like [mrgsolve::modlib]).
+##'
+##'
 ##' @importFrom mrgsolve mread
+##'
+##' @param x character name of a model in the depot
+##' @param ... passed to [mrgsolve::mread]
+##'
+##' @examples
+##'
+##' \dontrun{
+##'
+##' mod <- depot("gcsf")
+##'
+##' mod <- mread("moxi", depot())
+##' }
+##'
+##' @md
 ##' @export
 depot <- function(x = NULL, ...) {
   if(is.null(x)) return(system.file("models", package = "depot"))
